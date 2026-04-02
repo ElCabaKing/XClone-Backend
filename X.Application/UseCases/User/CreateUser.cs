ae27c53a2fd0497881212879091f50dd7b2407fb
ElCabaKing <davi.cabanilla@gmail.com>
2026-04-02 11:57:00 -0500

    removed the bad reference

--- X.Application/UseCases/User/CreateUser.cs
+++ X.Application/UseCases/User/CreateUser.cs
@@ -3,9 +3,9 @@ namespace X.Application.UseCases.User;
 using X.Application.DTOs.Command;
 using X.Application.DTOs.Response;
 using X.Domain.Entities;
-using X.Infrastructure.Repository;
+using X.Domain.Interfaces.Repository;
 
-public class CreateUser(UserRepository userRepository)
+public class CreateUser(IUserRepository userRepository)
 {
 
     public Task<int> Execute(CreateUserCommand command)
